28c3c8ba4568e2fe9dc76d00beb18fa9fd566da5
shan458 <77813639+shan458@users.noreply.github.com>
2021-05-13 16:11:03 -0700

    finished psuedocode

--- final.py
+++ final.py
@@ -5,3 +5,30 @@ Then a calculate_percent_above_average() function will be created to calculate t
 The grades in the input text file will be read using the open() method and then the grades will be assigned to a list of integers.
 Lastly the information, which contains the student grades, average grade, and the percentage of grades that are above average, will be outputted to the user.
 """
+
+"""
+def main()
+    file = Final.txt
+    calculate_percent_above_average(file)
+
+def calculate_percent_above_average(file)
+    open file final.txt
+    create list = each grade from list
+    close file
+    length = total grades from list
+    print("Number of grades:", length)
+    sum = sum of all grades from list
+    average = sum / length
+    print("Average grade:", avg)
+    counter = 0
+
+    for item in list
+        if item > avg
+            add item to count
+
+    percent Above = counter / length
+    print("Percent of grades above average:" , end="")
+    print("{0:.2f%}".format(percentAbove))
+
+main()
+"""
