7ce9757b95107d074a072e79ab57c7199f4f05cf
shan458 <77813639+shan458@users.noreply.github.com>
2021-05-13 16:15:13 -0700

    finished exam-end

--- final.py
+++ final.py
@@ -32,3 +32,26 @@ def calculate_percent_above_average(file)
 
 main()
 """
+
+def main():
+    file = "Final.txt"
+    calculate_percent_above_average(file)
+
+def calculate_percent_above_average(file):
+    infile = open(file, "r")
+    listGrades = [int(line.rstrip()) for line in infile]
+    infile.close()
+    length = len(listGrades)
+    sum1 = sum(listGrades)
+    avg = sum1 / length
+    print("Number of grades:", length)
+    print("Average grade:", avg)
+    counter = 0
+    for item in listGrades:
+        if item > avg:
+            counter += 1
+    percentAbove = counter / length
+    print("Percent of grades above average:", end =" ")
+    print("{0:.2%}".format(percentAbove))
+
+main()
